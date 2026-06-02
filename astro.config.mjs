1d7ffbc1f42b37723d0beb7c22062b7c05b660e0
tevibayu <tevi.bayu@gmail.com>
2026-06-02 16:51:39 +0700

    chore: update site URL domain to portalpintar.id

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.portalpintar.app',
+	site: 'https://docs.portalpintar.id',
 	output: 'static',
 	integrations: [
 		sitemap(),
